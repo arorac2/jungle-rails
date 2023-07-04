17d83bf695e6ce1e5da7fb6c343aa81f5c267dc9
Chaahat <arorac2@mymacewan.ca>
2023-07-04 16:58:39 -0600

    add testing to home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,17 @@
+describe('Home Page', () => {
+    it('Visits the home page', () => {
+      cy.visit('/'); 
+      cy.contains('Sign Up');
+      cy.contains('Log In');
+    });
+
+    it("There is products on the page", () => {
+        cy.get(".products article").should("be.visible");
+      });
+      it("There is 2 products on the page", () => {
+        cy.get(".products article").should("have.length", 2);
+      });
+      
+      
+
+  });
